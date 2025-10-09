92c31516e88e1e00ec6c7ea83f6991142caee510
Ygor <ygor.felix@gmail.com>
2025-10-08 22:52:54 -0300

    fiz alteração na lista para representar a jogado do computador, estudei sobre loop e irei usar o while, para controlar as jogadas

--- game.py
+++ game.py
@@ -5,6 +5,21 @@
 campo = [1,2,3,4,5,6,7,8,9]
 
 # função para imprimir o tabuleiro, para não precisar ficar repetindo o código
-def board():
-    print(f"{campo[0]} | {campo[1]} | {campo[2]} \n" f"{campo[3]} | {campo[4]} | {campo[5]}\n" f"{campo[6]} | {campo[7]} | {campo[8]}")
-print (board())
+def board(campo):
+    print(f"{campo[0]} | {campo[1]} | {campo[2]} \n" 
+          f"---------\n"
+          f"{campo[3]} | {campo[4]} | {campo[5]}\n" 
+          f"---------\n"
+          f"{campo[6]} | {campo[7]} | {campo[8]}")
+board(campo)
+
+# O computador joga na posição 5
+print("O computador jogou:")
+campo[4] = 'X'
+board(campo)
+
+jogada = int(input("Sua vez. Escolha um número: "))
+
+campo[jogada - 1] = 'O'
+print("Você jogou:")
+board(campo)
