8b49f0e27ad8276282950e3e9db7dc7ce74d0b98
Ygor <ygor.felix@gmail.com>
2025-10-09 21:44:54 -0300

    montei estruturas com while, mas preciso entender como vou montar o restante do código, tá um pouco confuso, wwhile dentro de outro while com if

--- game.py
+++ game.py
@@ -1,8 +1,18 @@
 # Jogo da velha
 # Autor: Ygor Felix
 
+#-------------------------------
+# --- 1.CONFIGURAÇÃO INICIAL ---
+#-------------------------------
+
 # criação de uma lista para representar o tabuleiro
 campo = [1,2,3,4,5,6,7,8,9]
+# variável para representar que o não acabou
+jogo_acabou = False 
+
+#--------------------------
+# --- 2.FUNÇÕES DO JOGO ---
+#--------------------------
 
 # função para imprimir o tabuleiro, para não precisar ficar repetindo o código
 def board(campo):
@@ -11,15 +21,61 @@ def board(campo):
           f"{campo[3]} | {campo[4]} | {campo[5]}\n" 
           f"---------\n"
           f"{campo[6]} | {campo[7]} | {campo[8]}")
-board(campo)
+
+# Função verificação de vitória, em linha, coluna e diagonal
+# utilizando if e colocando dentro de uma função
+def checar_vitoria():
+    if campo[0] == campo[1] == campo[2] or \
+        campo[3] == campo[4] == campo[5] or \
+        campo[6] == campo[7] == campo[8] or \
+        campo[0] == campo[3] == campo[6] or \
+        campo[1] == campo[4] == campo[7] or \
+        campo[2] == campo[5] == campo[8] or \
+        campo[0] == campo[4] == campo[8] or \
+        campo[2] == campo[4] == campo[6]:
+        # Usando return para retornar True ou False, ao invés de print
+        # se qualquer umas dessas condições forem verdadeiras, retorna True
+        return True
+    return False
+
+#--------------------------
+# --- 3.JOGO PRINCIPAL ----
+#--------------------------
 
 # O computador joga na posição 5
+print("Bem vindo ao jogo da velha!")
+print("O computador é o X e você é o O")
 print("O computador jogou:")
 campo[4] = 'X'
-board(campo)
 
-jogada = int(input("Sua vez. Escolha um número: "))
+while not jogo_acabou:
+    # mostra o tabuleiro atual
+    board(campo)
 
-campo[jogada - 1] = 'O'
-print("Você jogou:")
-board(campo)
+    # ---Vez do Jogador ---
+
+    # Loop para garantir que o jogador escolha uma posição válida
+    while True:
+        jogada = int(input("Sua vez. Escolha um número: "))
+        posicao = jogada - 1
+
+        # checa se a casa está disponível
+        if campo[posicao] not in ['X', 'O']:
+            campo[posicao] = 'O'
+            break
+        else:
+            print("Posição inválida. Tente novamente.") 
+
+    # checa se o jogador ganhou / acabou
+    vencedor = checar_vitoria()
+    if vencedor == "O":
+        print("Parabéns! Você ganhou!")
+        jogo_acabou = True
+    elif vencedor == "X":
+        print("O computador ganhou!")
+        jogo_acabou = True
+
+    # --- Fim de Jogo ---
+    board(campo)
+    print("Fim de jogo!")
+
